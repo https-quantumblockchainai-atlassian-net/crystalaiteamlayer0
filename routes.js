6a0a391e72cff28989049254bee4a6e3f0d7af6b
root <root@ip-10-0-81-49.ec2.internal>
2022-07-23 08:27:36 +0000

    Copy Template Repo

---
+++ routes.js
@@ -0,0 +1,21 @@
+// This file was added by layer0 init.
+// You should commit this file to source control.
+
+import { Router } from '@layer0/core/router'
+
+export default new Router()
+  .static('public', ({ cache }) => {
+    cache({
+      edge: {
+        maxAgeSeconds: 60 * 60 * 60 * 365,
+        forcePrivateCaching: true,
+      },
+      browser: {
+        maxAgeSeconds: 0,
+        serviceWorkerSeconds: 60 * 60 * 24,
+      },
+    })
+  })
+  .fallback(({ appShell }) => {
+    appShell('public/index.html')
+  })
